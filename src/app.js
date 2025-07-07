5e5c2a3f51c9374aa2f5d9c697ba9cfd7717a630
Ajay Kumar <ajayk066294@gmail.com>
2025-07-07 11:12:54 +0530

    update app.js and some utils files

--- src/app.js
+++ src/app.js
@@ -9,13 +9,21 @@ app.use(cors({
     credential : true
 }));
 
-app.use(express.json({limit: "16kb " }))
+app.use(express.json({limit: '16kb' }))
 
-app.use(express.urlencoded({extend: true,limit: "16kb"}))
+app.use(express.urlencoded({extend: true,limit: '16kb'}))
 
 app.use(express.static("public"))
 
 app.use(cookieParser())
 
 
-export { app }
+// routers
+import  userRouter from './routers/user.router.js'
+ 
+// routes declaration 
+
+app.use("/api/v1/user", userRouter)
+
+
+export { app } 
